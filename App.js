b6de6cb409a8c00256ab992e6175743b1c7b7d0c
Siva20021 <88962500+Siva20021@users.noreply.github.com>
2022-10-07 18:28:09 +0530

    Initialized repository

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import SplashScreen from "./screens/SplashScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import SplashScreen2 from "./screens/SplashScreen2";
+import SplashScreen3 from "./screens/SplashScreen3";
+import SplashScreen4 from "./screens/SplashScreen4";
+import SplashScreen5 from "./screens/SplashScreen5";
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="SplashScreen"
+          component={SplashScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SplashScreen2"
+          component={SplashScreen2}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SplashScreen3"
+          component={SplashScreen3}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SplashScreen4"
+          component={SplashScreen4}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SplashScreen5"
+          component={SplashScreen5}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // flex: 1,
+    // backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
   },
 });
